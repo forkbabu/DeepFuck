5c049ac14a337059459bb6bc5c0d8ff931e6d58b
Forkbabu <sayantandas30011998@gmail.com>
2020-09-05 20:07:09 +0530

    Create git-setup.sh

---
+++ git-setup.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/AliaksandrSiarohin/first-order-model
+cd first-order-model
+wget https://github.com/forkbabu/dustbin/blob/master/jadoo.mp4?raw=true
+mv jadoo.mp4?raw=true jadoo.mp4
+wget https://cdn.britannica.com/58/129958-050-C3FE2DD2/Adolf-Hitler-1933.jpg
+
