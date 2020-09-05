27d48f8895e677dd8d70dd372d1f47c6b9639d17
Forkbabu <sayantandas30011998@gmail.com>
2020-09-05 20:12:34 +0530

    Update git-setup.sh

--- git-setup.sh
+++ git-setup.sh
@@ -3,4 +3,6 @@ cd first-order-model
 wget https://github.com/forkbabu/dustbin/blob/master/jadoo.mp4?raw=true
 mv jadoo.mp4?raw=true jadoo.mp4
 wget https://cdn.britannica.com/58/129958-050-C3FE2DD2/Adolf-Hitler-1933.jpg
+alias gdrive.sh='curl gdrive.sh | bash -s'
+gdrive.sh 1AuO54cgO3S7JKCauudbK1xb6odfnQG3T
 
